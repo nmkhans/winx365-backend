85bd75db2f5551e756cb61eaf00fafc94f632904
nmkhans <nurmoin05@gmail.com>
2023-01-17 22:35:14 +0600

    database connection completed

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require("express");
 require("dotenv").config();
 const router = require("./src/routes/routes");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 //? App confuguration
 const app = express();
 app.use(cors());
 app.use(express.json())
+
+//? database configuration
+const uri = process.env.DB_URI;
+
+const databaseConfig = {
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS
+}
+
+//? database connection
+mongoose.set("strictQuery", true);
+mongoose.connect(uri, databaseConfig)
+    .then(() => console.log("database connected"))
+    .catch(error => console.log(error))
 
 //? handle routes
 app.use("/api/v1", router);
